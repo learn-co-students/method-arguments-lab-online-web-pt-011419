9016978b13bf829e174599447dc38ffab700615b
cultured-sequence-b2a8 <britt90609@gmail.com>
2021-05-05 15:40:01 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,16 +1,16 @@
 name = "Josh"
 
-
 def introduction(name)
   puts "Hi, my name is #{name}."
-end 
+end
 
 introduction(name)
 
 name = "Dan"
 language = "Ember.js"
-def introduction_with_name(name, language)
+
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_name(name, language)
+introduction_with_language(name, language)
